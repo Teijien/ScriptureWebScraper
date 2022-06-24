d1ac03cea81efdad3fbd32b5bbe0ca06f65f067f
Teijien <tjhansen@ucsc.edu>
2022-06-23 20:24:36 -0700

    Removed lettered annotations from fetched text

--- main/ScriptureScraper.py
+++ main/ScriptureScraper.py
@@ -3,7 +3,7 @@ import PageRequester
 
 if __name__ == '__main__':
     page = PageRequester.request_page("https://www.churchofjesuschrist.org/study/scriptures/ot/2-sam/1?lang=eng")
-    verses = TextScraper.verse_parser(page, 'div', "verse", "body")
+    verses = TextScraper.verse_parser(page, 'html.parser', 'div', "verse", 'a', 'sup', "para-mark", "body")
 
     for v in verses:
-        print(v)
+        print(v.get_text())
--- main/TextScraper.py
+++ main/TextScraper.py
@@ -1,20 +1,25 @@
 from bs4 import BeautifulSoup
 
 
-def verse_parser(page, tag, verse_class, body_class=None):
-    soup = BeautifulSoup(page.content, 'html.parser')
+def verse_parser(page, parser_type, div_tag, verse_class, a_tag, sup_tag, paramark_class, body_class=None):
+    soup = BeautifulSoup(page.content, parser_type)
 
     if body_class is not None:
-        body = soup.find(tag, class_=body_class)
+        body = soup.find(div_tag, class_=body_class)
     else:
-        body = soup.find(tag)
+        body = soup.find(div_tag)
 
     scripture = body.find_all(class_=verse_class)
+
+    # Remove all marker tags from the fetched html
+    for v in scripture:
+        for a in v.find_all(a_tag):
+            a.find(sup_tag).decompose()
 
     verses = [""] * len(scripture)
 
     # Put the text into a clean format
     for i in range(len(scripture)):
-        verses[i] = scripture[i].get_text()
+        verses[i] = scripture[i]
 
     return verses
